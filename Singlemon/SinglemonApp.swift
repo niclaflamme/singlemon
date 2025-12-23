2a2d4bef60b64fd1de415d7bc0487f3203425ffc
Nic Laflamme <nic@laflamme.io>
2025-12-23 11:25:37 -0700

    Initial Commit

---
+++ Singlemon/SinglemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglemonApp.swift
+//  Singlemon
+//
+//  Created by Nic on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct SinglemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
